fc62aecaa7dee8cc5aa473d042c654157d41e3bc
Gracesta <jayblizrage@gmail.com>
2023-05-11 00:46:34 +0900

    userID to identify valid connection

--- static/chatScript.js
+++ static/chatScript.js
@@ -6,6 +6,33 @@ let lastMessageTime = null;
 const userId = localStorage.getItem('userId');
 console.log("user id:", userId); // Output: myValue
 
+if (!userId) {
+  // User is not logged in, redirect to login page
+  window.history.replaceState(null, null, "/");
+  window.location.href = "/";
+}else{
+  // set up websocket connection for authenticated user
+  // Get the current port number
+  var port = window.location.port;
+  var wsUrl = "ws://" + window.location.hostname + ":" + port + "/ws?userId="+userId;
+  var socket = new WebSocket(wsUrl);
+
+  // Websocket to supervise message from backend
+  socket.addEventListener('message', function(event) {
+    const message = event.data;
+    // Handle incoming message from server
+    console.log('Received message:', message);
+    const bubble = document.createElement('div');
+    const userName = message.split("|")[1]
+    const content = message.split("|")[2]
+    bubble.classList.add('chat-bubble', 'other-bubble', 'incoming-bubble');
+    bubble.innerHTML = `<p><strong>${userName}</strong></p><p>${content}</p>`;
+    chatWindow.appendChild(bubble);
+    // Scroll to the bottom of the chat window when a new message is added
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+  });
+}
+
 // function createBubbleForMessageFromUser(message, user, bubbleOwnerClass, bubbleFromClass){
 //   const bubble = document.createElement('div');
 //   bubble.classList.add('chat-bubble', bubbleOwnerClass, bubbleFromClass);
@@ -16,36 +43,30 @@ console.log("user id:", userId); // Output: myValue
 // }
 
 // Get the current port number
-var port = window.location.port;
-var wsUrl = "ws://" + window.location.hostname + ":" + port + "/ws?userId="+userId;
-var socket = new WebSocket(wsUrl);
+// var port = window.location.port;
+// var wsUrl = "ws://" + window.location.hostname + ":" + port + "/ws?userId="+userId;
+// var socket = new WebSocket(wsUrl);
 
-// // Set the custom header
-// socket.addEventListener('open', function(event) {
-//   // global argument userID
-//   socket.setRequestHeader('X-User-Id', userId);
+// // Websocket to supervise message from backend
+// socket.addEventListener('message', function(event) {
+//   const message = event.data;
+//   // Handle incoming message from server
+//   console.log('Received message:', message);
+//   const bubble = document.createElement('div');
+//   const userName = message.split("|")[1]
+//   const content = message.split("|")[2]
+//   bubble.classList.add('chat-bubble', 'other-bubble', 'incoming-bubble');
+//   bubble.innerHTML = `<p><strong>${userName}</strong></p><p>${content}</p>`;
+//   chatWindow.appendChild(bubble);
+//   // Scroll to the bottom of the chat window when a new message is added
+//   chatWindow.scrollTop = chatWindow.scrollHeight;
 // });
-
-
-// Websocket to supervise message from backend
-socket.addEventListener('message', function(event) {
-  const message = event.data;
-  // Handle incoming message from server
-  console.log('Received message:', message);
-  const bubble = document.createElement('div');
-  const userName = message.split("|")[1]
-  const content = message.split("|")[2]
-  bubble.classList.add('chat-bubble', 'other-bubble', 'incoming-bubble');
-  bubble.innerHTML = `<p><strong>${userName}</strong></p><p>${content}</p>`;
-  chatWindow.appendChild(bubble);
-  // Scroll to the bottom of the chat window when a new message is added
-  chatWindow.scrollTop = chatWindow.scrollHeight;
-});
 
 // HANDLE the message sent by user: (1) send to backend （2）Repeat this information in chat window
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const text = messageInput.value.trim();
+    
     const timestamp = new Date().toISOString(); // Get current timestamp
 
     // Deal with message input by user
